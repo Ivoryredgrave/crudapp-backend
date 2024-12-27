e3242ea31c58bf5e7961d7282d3ffc3101f5a1d8
Ivoryredgrave <ivoryredgrave@gmail.com>
2024-12-26 23:32:28 -0400

    Minor changes

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -2,7 +2,7 @@
 const {
     DATABASE_URL: database_url = 'postgresql://postgres:ROOT@localhost:5432/crudapp',
     PORT: port = '3000',
-    CORS_ORIGIN: cors_origin = 'https://ivoryredgrave.github.io/crudapp-frontend'
+    CORS_ORIGIN: cors_origin = 'http://localhost:5173'
 
 } = process.env
 
